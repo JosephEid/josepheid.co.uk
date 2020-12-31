25353085b6152fa6e4e0645119834a619a18a071
JosephEid <josephceid@gmail.com>
2020-12-31 01:47:04 +0000

    Update bio

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -7,14 +7,17 @@ function About () {
       <div className="About__Row">
         <div className="About__Col-9">
           <h2>About Me</h2>
-          <p>Hello! I am a graduate from the University of Sheffield who has been working in London as a Software Engineer since 2019.
-            I love building all things software, including web apps, extensions and games.</p>
+          <p>Hello! I am a graduate from the University of Sheffield who has been working in London as a Software Engineer since 2019.</p>
+          <h2>Skills</h2>
+          <p>
+            Things I love building: Cloud-based Web Apps, Cloud-based Automated Test Suites, Extensions and Games.
+            Topics I love discussing: Cyber Security, Cloud Software.
+            Technologies I tend to use: C#, JavaScript, Python, AWS, React and Godot
+          </p>
           <div className="row">
             <div className="columns contact-details">
               <h2>Contact me by email:</h2>
-              <p className="address">
-                <span>josephceid@gmail.com</span>
-              </p>
+              <span>josephceid@gmail.com</span>
             </div>
           </div>
         </div>
