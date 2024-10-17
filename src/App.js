b4485451a2abc333b6db44ab52c08078a2a7f6af
Joseph Eid <josephceid@gmail.com>
2024-10-17 23:28:18 +0100

    fix build

--- src/App.js
+++ src/App.js
@@ -3,20 +3,16 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './Components/Home/Home'
 
 import './App.css'
-import HappyBirthday from './Components/HappyBirthday/HappyBirthday'
-import HappyBirthday2 from './Components/HappyBirthday2/HappyBirthday2'
 
 function App () {
   return (
-    <BrowserRouter>
-      <div className='App'>
-        <Routes>
-          <Route path='/happyBirthday' element={<HappyBirthday />} />
-          <Route path='/happyBirthdayHannah' element={<HappyBirthday2 />} />
-          <Route path='/' element={<Home />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+        <BrowserRouter>
+            <div className='App'>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                </Routes>
+            </div>
+        </BrowserRouter>
   )
 }
 
--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -4,86 +4,56 @@ import './About.css'
 
 const About = () => {
   return (
-    <section id="about" className="About">
-      <Container>
-        <Row>
-          <Col>
-            <h2>About Me</h2>
-            <p>Hello! I am a graduate from the University of Sheffield who has been working in London as a Software Engineer since 2019.</p>
-          </Col>
-          <Col>
-            <img
-              className="About__ProfilePic"
-              src='myAvatar.png'
-              alt="Avatar"
-            />
-            <h3>Contact me by email:</h3>
-            <p>josephceid@gmail.com</p>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <h3>Things I love to build:</h3>
-            <ul>
-              <li>
-                Web Apps
-              </li>
-              <li>
-                Automated Test Suites
-              </li>
-              <li>
-                Extensions
-              </li>
-              <li>
-                Games
-              </li>
-            </ul>
-          </Col>
-          <Col>
-            <h3>Topics I&apos;m interested in:</h3>
-            <ul>
-              <li>
-                Cyber Security
-              </li>
-              <li>
-                Cloud Technologies
-              </li>
-              <li>
-                Game Development
-              </li>
-              <li>
-                Latest Software Development Tools
-              </li>
-            </ul>
-          </Col>
-          <Col>
-            <h3>Technologies I tend to use:</h3>
-            <ul>
-              <li>
-                C#
-              </li>
-              <li>
-                JavaScript
-              </li>
-              <li>
-                Python
-              </li>
-              <li>
-                AWS
-              </li>
-              <li>
-                Docker
-              </li>
-              <li>
-                React
-              </li>
-              <li>
-                Godot
-              </li>
-            </ul>
-          </Col>
-        </Row>
-      </Container>
+        <section id='about' className='About'>
+            <Container>
+                <Row>
+                    <Col>
+                        <h2>About Me</h2>
+                        <p>
+                            Hello! I am a graduate from the University of
+                            Sheffield who has been working in London as a
+                            Software Engineer since 2019.
+                        </p>
+                    </Col>
+                    <Col>
+                        <img
+                            className='About__ProfilePic'
+                            src='myAvatar.png'
+                            alt='Avatar'
+                        />
+                        <h3>Contact me by email:</h3>
+                        <p>josephceid@gmail.com</p>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <h3>Things I love to build:</h3>
+                        <ul>
+                            <li>Cloud Based Web Applications</li>
+                            <li>Automated Test Suites</li>
+                            <li>Extensions</li>
+                            <li>Games</li>
+                        </ul>
+                    </Col>
+                    <Col>
+                        <h3>Topics I&aposm interested in:</h3>
+                        <ul>
+                            <li>All things in the javascript ecosystem</li>
+                            <li>Cloud Technologies</li>
+                        </ul>
+                    </Col>
+                    <Col>
+                        <h3>Technologies I tend to use:</h3>
+                        <ul>
+                            <li>Go</li>
+                            <li>Node.js</li>
+                            <li>React</li>
+                            <li>AWS</li>
+                            <li>Next.js</li>
+                        </ul>
+                    </Col>
+                </Row>
+            </Container>
     </section>
   )
 }
